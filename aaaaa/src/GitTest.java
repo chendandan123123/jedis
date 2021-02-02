6bfeda4277870a9e0f5a6e5f3c8a7f64fd3241c8
陈丹 <2297746381@qq.com>
2021-02-02 16:15:31 +0800

    提交jedis本地仓库02

--- aaaaa/src/GitTest.java
+++ aaaaa/src/GitTest.java
@@ -2,6 +2,6 @@ public class GitTest {
 
 
     public static void main(String[] args) {
-        System.out.println("aaaaaaaa");
+        System.out.println("aaaaaaaabbbbbbbbbbbbbbbbccccccccc");
     }
 }
